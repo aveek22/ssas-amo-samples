c3c81e70ca16bad30a9cc7e5cb3e0f25bb688d5f
Aveek <aveek.d22@gmail.com>
2019-09-13 13:22:31 +0530

    Commented out querying the cube

--- SSAS_ConsoleApp/Program.cs
+++ SSAS_ConsoleApp/Program.cs
@@ -73,7 +73,7 @@ namespace SSAS_ConsoleApp
 
             Console.WriteLine();
             Console.WriteLine("Trying to read data from cube");
-            GetData(connectionString);
+            //GetData(connectionString);
 
 
 
